4e0b28cd934c3edc2f5703cc9d68b1f91de3c6df
Ram Kumar Dhimal <ramkumardhimal2058@gmail.com>
2025-03-17 21:02:44 +1100

    search function on the borrows List and the student list

--- src/pages/borrows/Borrows.jsx
+++ src/pages/borrows/Borrows.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import UserLayout from "../../components/layout/UserLayout";
 import { useDispatch, useSelector } from "react-redux";
 import BurrowBlock from "./BurrowBlock.jsx";
@@ -8,15 +8,43 @@ const Borrows = () => {
   const dispatch = useDispatch();
   const { borrows } = useSelector((state) => state.borrows);
 
+  const [searchedData, setSearchedData] = useState([]);
+  const [displayBorrows, setDisplayBorrows] = useState([]);
+
+  const handleOnSearch = (e) => {
+    setSearchedData(e.target.value);
+    const filteredData = borrows.filter((item) => {
+      return (
+        item.title.toLowerCase().includes(e.target.value.toLowerCase()) ||
+        item.status.toLowerCase().includes(e.target.value.toLowerCase()) ||
+        item.dueDate.toString().includes(e.target.value.toString())
+      );
+    });
+
+    setDisplayBorrows(filteredData);
+  };
+
   useEffect(() => {
     dispatch(getBorrowAction());
-  });
+    setDisplayBorrows(borrows);
+  }, []);
   return (
     <UserLayout pageTitle="Burrow List">
       <div className="w-100">
-        {borrows.length} burrowed history found!
+        <div className="d-flex justify-content-end">
+          <input
+            type="search"
+            name="search"
+            id="search"
+            className="p-1 mb-3 rounded"
+            placeholder="Search Borrows ..."
+            value={searchedData}
+            onChange={handleOnSearch}
+          />
+        </div>
+        {displayBorrows.length} burrowed history found!
         <hr />
-        <BurrowBlock borrows={borrows} />
+        <BurrowBlock borrows={displayBorrows} />
       </div>
     </UserLayout>
   );
--- src/pages/students/Students.jsx
+++ src/pages/students/Students.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import UserLayout from "../../components/layout/UserLayout";
 import { useDispatch, useSelector } from "react-redux";
 import { getStudentsAction } from "../../features/users/userAction";
@@ -7,14 +7,41 @@ import StudentsTable from "./StudentsTable";
 const Students = () => {
   const dispatch = useDispatch();
   const { students } = useSelector((state) => state.users);
+  const [searchedData, setSearchedData] = useState([]);
+  const [displayStudents, setdisplayStudents] = useState([]);
+
+  const handleOnSearch = (e) => {
+    setSearchedData(e.target.value);
+    const filteredData = students.filter((item) => {
+      return (
+        item.fName.toLowerCase().includes(e.target.value.toLowerCase()) ||
+        item.lName.toLowerCase().includes(e.target.value.toLowerCase())
+      );
+    });
+    setdisplayStudents(filteredData);
+  };
+
   useEffect(() => {
     dispatch(getStudentsAction());
+    setdisplayStudents(students);
   }, []);
   console.log(1111, students);
   return (
     <UserLayout pageTitle="Students">
       <div className="w-100">
-        <StudentsTable students={students} />
+        <div className="d-flex justify-content-end">
+          <input
+            type="search"
+            name="search"
+            id="search"
+            className="mb-3 rounded p-1 "
+            placeholder="Search Students ..."
+            value={searchedData}
+            onChange={handleOnSearch}
+          />
+        </div>
+        <p>{displayStudents.length} student(s) Found !</p>
+        <StudentsTable students={displayStudents} />
       </div>
     </UserLayout>
   );
